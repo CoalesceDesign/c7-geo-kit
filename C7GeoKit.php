ba59ac63da80f8354c8d17818cf3daf48f80aa72
Tracy Floyd <tracy@coalesce.net>
2018-04-13 09:52:28 -0400

    Initial import

---
+++ C7GeoKit.php
@@ -0,0 +1,54 @@
+<?php
+class C7GeoKit
+{
+
+	// =====================================================================================================
+	/*
+	* Get array of all U.S. states
+	* @param array $options
+	* @return array
+	*/
+	function getStates( $options )
+	{
+
+	}
+
+
+	// =====================================================================================================
+	/*
+	* Get array of U.S. state capitals, indexed by 2-letter state code
+	* @param array $options (array of state codes to get capitals for)
+	* @return array
+	*/
+	function getStateCapitals( $options )
+	{
+
+	}
+
+
+	// =====================================================================================================
+	/*
+	* Get capital of specific U.S. state
+	* @param array $options
+	* @return string
+	*/
+	function getStateCapital( $stateCode )
+	{
+
+	}
+
+
+	// =====================================================================================================
+	/*
+	* Get array of U.S. state neighbors, indexed by 2-letter state code
+	* @param array $options
+	* @return array
+	*/
+	function getStateNeighbor( $stateCode )
+	{
+
+	}
+
+
+
+} # End C7GeoKit
